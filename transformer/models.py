45c34d8f6187105ac20ae9653888e1e11414bc87
amirHosseinS3 <ahns3.ahe@gmail.com>
2021-07-22 17:11:03 -0400

    Auto encoder model for image added

--- transformer/models.py
+++ transformer/models.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 from utils import *
 
 
-
 class Attention(tf.keras.layers.Layer):
     def __init__(self, dA, dG):
         super(Attention, self).__init__()
@@ -18,138 +17,57 @@ class Attention(tf.keras.layers.Layer):
         q = self.wq(q)
         k = self.wk(k)
         v = self.wv(v)
-        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v)
+        scaled_attention = scaled_dot_product_attention(q, k, v)
         output = self.dense(scaled_attention)
-        return output, attention_weights
+        return output
 
 
-
-class EncoderFeatureExtraction(tf.keras.layers.Layer):
-    def __init__(self, dG):
-        super(EncoderFeatureExtraction, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(dG, (1,1))
-        self.conv2 = tf.keras.layers.Conv2D(dG, (1,1))
-
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        return x
-
-
-
-class DecoderPredictionLayer(tf.keras.layers.Layer):
-    def __init__(self, dG):
-        super(DecoderPredictionLayer, self).__init__()
-        self.conv = tf.keras.layers.Conv2D(dG, (1,1))
-        self.dense = tf.keras.layers.Conv2D(3, (1,1))
-
-    def call(self, x):
-        x = self.conv(x)
-        x = self.dense(x)
-        return x
-
-        
-
-class DecoderContextLayer(tf.keras.layers.Layer):
-    def __init__(self, M, W, H, dG):
-        super(DecoderContextLayer, self).__init__()
-        self.M = M
-        self.W = W
-        self.H = H
-        self.dense1 = tf.keras.layers.Dense(M, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(W*H)
-        self.conv = tf.keras.layers.Conv2D(dG, (1,1))
-        
-    def call(self, x):
-        x = self.dense1(x)
-        x = x[:, :, tf.newaxis]
-        x = self.dense2(x)
-        x = tf.reshape(x, (-1, self.M, self.W, self.H))
-        x = x[:, :, :, :, tf.newaxis]
-        x = self.conv(x)
-        return x
-
-
-
-class SpatialLayer(tf.keras.layers.Layer):
+class AttentionBlock(tf.keras.layers.Layer):
     def __init__(self, dA, dG):
-        super(SpatialLayer, self).__init__()
+        super(AttentionBlock, self).__init__()
         self.att = Attention(dA, dG)
         self.feed_forward_network = point_wise_feed_forward_network(dG)
 
     def call(self, x):
-        s_out, s_att = self.att(x, x, x)
-        out = self.feed_forward_network(s_out)
-        return out + x, s_att
+        out = self.att(x, x, x)
+        out = self.feed_forward_network(out)
+        return out
 
 
+class AEConvolutionalEncoder(tf.keras.Model):
+    def __init__(self):
+        super(AEConvolutionalEncoder).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(16, (5,5), padding='same', strides=2, activation='relu')
+        self.conv2 = tf.keras.layers.Conv2D(32, (5,5), padding='same', strides=2, activation='relu')
+        self.conv3 = tf.keras.layers.Conv2D(32, (5,5), padding='same', strides=1, activation='relu')
+        self.conv4 = tf.keras.layers.Conv2D(32, (5,5), padding='same', strides=1, activation='relu')
+        self.conv5 = tf.keras.layers.Conv2D(10, (5,5), padding='same', strides=1, activation='relu')
+        self.flatten = tf.keras.layers.Flatten()
 
-class TemporalLayer(tf.keras.layers.Layer):
-    def __init__(self, dA, dG):
-        super(TemporalLayer, self).__init__()
-        self.att = Attention(dA, dG)
-        self.feed_forward_network = point_wise_feed_forward_network(dG)
-
-    def call(self, x):
-        t_out, t_att = self.att(x, x, x)
-        out = self.feed_forward_network(t_out)
-        return out + x, t_att
-
-
-
-class SpatialTemporalBlock(tf.keras.layers.Layer):
-    def __init__(self, dA, dG):
-        super(SpatialTemporalBlock, self).__init__()
-        self.spatial = SpatialLayer(dA, dG)
-        self.temporal = TemporalLayer(dA, dG)
-
-    def call(self, x):
-        x, s_att = self.spatial(x)
-        x, t_att = self.temporal(x)
-        return x, s_att, t_att
-
-
-
-class EncoderContextLayer(tf.keras.layers.Layer):
-    def __init__(self, compr_size):
-        super(EncoderContextLayer, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(10, (1,1))
-        self.conv2 = tf.keras.layers.Conv2D(20, (1,1))
-        self.dense = tf.keras.layers.Dense(compr_size, activation='sigmoid')
-        
     def call(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = tf.reshape(x, (x.shape[0], -1))
-        x = self.dense(x)
-        return x
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.flatten(x)
 
 
-
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, dA, dG, compr_size):
-        super(Encoder, self).__init__()
-        self.feat_layer = EncoderFeatureExtraction(dG)
-        self.stb = SpatialTemporalBlock(dA, dG)
-        self.enc_layer = EncoderContextLayer(compr_size)
-
-    def call(self, x):
-        x = self.feat_layer(x)
-        x, s_att, t_att = self.stb(x)
-        x = self.enc_layer(x)
-        return x, s_att, t_att
-
-
-
-class Decoder(tf.keras.layers.Layer):
-    def __init__(self, dA, dG, M, W, H):
-        super(Decoder, self).__init__()
-        self.dec_context = DecoderContextLayer(M, W, H, dG)
-        self.stb = SpatialTemporalBlock(dA, dG)
-        self.dec_layer = DecoderPredictionLayer(dG)
+class AEConvolutionalDecoder(tf.keras.Model):
+    def __init__(self, M, W, H):
+        super(AEConvolutionalDecoder).__init__()
+        self.reshape1 = tf.keras.layers.Reshape((56, 56, -1))
+        self.conv1t = tf.keras.layers.Conv2DTranspose(32, (5,5), padding='same', strides=1, activation='relu')
+        self.conv2t = tf.keras.layers.Conv2DTranspose(32, (5,5), padding='same', strides=1, activation='relu')
+        self.conv3t = tf.keras.layers.Conv2DTranspose(32, (5,5), padding='same', strides=1, activation='relu')
+        self.conv4t = tf.keras.layers.Conv2DTranspose(16, (5,5), padding='same', strides=2, activation='relu')
+        self.conv5t = tf.keras.layers.Conv2DTranspose(3*M, (5,5), padding='same', strides=2, activation='sigmoid')
+        self.reshape2 = tf.keras.layers.Reshape((M, W, H, 3))
 
     def call(self, x):
-        x = self.dec_context(x)
-        x, s_att, t_att = self.stb(x)
-        x = self.dec_layer(x)
-        return x, s_att, t_att
+        x = self.reshape1(x)
+        x = self.conv1t(x)
+        x = self.conv1t(x)
+        x = self.conv1t(x)
+        x = self.conv1t(x)
+        x = self.reshape2(x)
--- transformer/utils.py
+++ transformer/utils.py
@@ -5,16 +5,16 @@ from models import *
 
 def scaled_dot_product_attention(q, k, v):
     matmul_qk = tf.matmul(q, k, transpose_b=True)
-    d_A = tf.cast(tf.shape(k)[-1], tf.float32)
-    scaled_attention_logits = matmul_qk / tf.math.sqrt(d_A)
+    dA = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dA)
     attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
     output = tf.matmul(attention_weights, v)
-    return output, attention_weights
+    return output
 
 
 def loss_function(real, pred):
     loss = tf.square(tf.cast(real, dtype=tf.float32) - tf.cast(pred, dtype=tf.float32))
-    return tf.reduce_sum(loss)
+    return tf.reduce_mean(loss)
 
 
 def point_wise_feed_forward_network(d_G):
